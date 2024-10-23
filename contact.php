a34c723dc0969cdefd5eff6a3466a082f263b61d
linh080504 <linhnguyenthuy0805@gmail.com>
2024-10-23 13:08:23 +0700

    Initial commit

---
+++ contact.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TJ Hotel - CONTACT</title>
+    <?php require('inc/links.php')?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+</head>
+<body class="bg-light">
+<?php require('inc/header.php');?>
+
+<div class="md-5 px-4">
+    <h2 class="fw-bold h-font text-center ">CONTACT US</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center  mt-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Corporis, placeat odio. Odit, perferendis <br> modi at illo possimus praesentium nam fugiat a, 
+        nulla tempora minus quia quos? Doloribus odio animi cum.
+    </p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4">
+                <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115330.43462063557!2d81.80158454999999!3d25.40226375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2zQWxsYWhhYmFkLCBVdHRhciBQcmFkZXNoLCDhuqRuIMSQ4buZ!5e0!3m2!1svi!2s!4v1729621422533!5m2!1svi!2s" height="450" loading="lazy"></iframe>
+                <h5>Address</h5>
+                <a href="https://maps.app.goo.gl/vWYRwtBmPNUFCUH3A" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+                    <i class="bi bi-geo-alt-fill"></i> XYZ, Prayaraj, Uttar Pradesh
+                </a>
+                <h5 class="mt-4">Call us</h5>
+                <a href="tell: +84123123123" class="d-inline-block mb-2 text-decoration-none text-dark ">
+                    <i class="bi bi-telephone-fill"></i>+84123123123
+                </a>
+                <br>
+                <a href="tell: +84123123123" class="d-inline-block mb-2 text-decoration-none text-dark ">
+                    <i class="bi bi-telephone-fill"></i>+84123123123
+                </a>
+                <h5 class="mt-4">Email</h5>
+                <a href="mailto: ask.tjweb@gmial.com" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-envelope"></i> ask.tjweb@gmail.com
+                </a>
+                <h5 class="mt-4">Follow us</h5>
+                <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                      <i class="bi bi-twitter me-1"></i>
+                </a>
+                
+                <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                    <i class="bi bi-facebook me-1"></i>
+                </a>
+        
+                <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                   <i class="bi bi-instagram me-1"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 ">
+                <form>
+                    <h5>Send a message</h5>
+                    <div class="mt-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control shadow-none">
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control shadow-none">
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label">Subject</label>
+                        <input type="email" class="form-control shadow-none">
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label">Message</label>
+                        <textarea class="form-control shadow-none" rows = "5" style="resize: none;"></textarea>
+                    </div>
+                    <button type="submit" class="btn text-white custom-bg mt-3 shadow-none">SEND </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require('inc/footer.php')?>
+</body>
+</html>
